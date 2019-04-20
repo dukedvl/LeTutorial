2cdad1c005b6365b56abf6f3268e90d6222bbd4c
Jeff B <dukedvl@gmail.com>
2019-04-20 10:41:44 -0400

    adding comments

--- basic_in_out.cpp
+++ basic_in_out.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 int main()
 {
+//Basic Console output
 cout<<"Hello World!\n What is your name?\n";
 
+//Basic Console input
 string name;
 cin >> name;
 
+//Basic Console output
+
 cout<<"Hello "<<name<<"!\n";
+
+//Return exit code (0) Success, indicating things went smoothly
 return EXIT_SUCCESS;
 }
