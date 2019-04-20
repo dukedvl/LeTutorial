932393c02f896fbadc95820cd8bca8ff1117fc52
Jeff B <dukedvl@gmail.com>
2019-04-20 10:35:48 -0400

    basic in/out C++

---
+++ basic_in_out.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+cout<<"Hello World!\n What is your name?\n";
+
+string name;
+cin >> name;
+
+cout<<"Hello "<<name<<"!\n";
+return EXIT_SUCCESS;
+}
